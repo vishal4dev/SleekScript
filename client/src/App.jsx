a1345dbaec3d570f609a752d7da1af68218c18db
vishiidevs <vspark007@gmail.com>
2024-05-07 01:02:10 +0530

    created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import {BrowserRouter, Routes , Route} from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Signin from './Signin'
+import Signup from './Signup'
+import Dashboard from './Dashboard'
+import Projects from './Projects'
 
 export default function App() {
   return (
-       <h1 className='text-3xl text-red-500'>Romeio </h1>
-  )
+   
+   <BrowserRouter>
+     <Routes>
+      
+       <Route path='/' element={<Home />} />
+       <Route path='/about' element={<About />} />
+       <Route path='/sign-in' element={<Signin />} />
+       <Route path='/sign-up' element={<Signup />} />
+       <Route path='/dashboard' element={<Dashboard />} />
+       <Route path='/projects' element={<Projects />} />
+      
+      </Routes>     
+   </BrowserRouter>
+
+   )
 }
